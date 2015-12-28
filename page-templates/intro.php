557eb772feacaa29077026895db7fa04e85c49e1
Hamed Nourhani <info@itstar.ir>
2015-12-28 10:37:12 +0330

    first commit

--- page-templates/intro.php
+++ page-templates/intro.php
@@ -9,8 +9,9 @@ get_header(); ?>
     <main class="site-main intro-main">
         <?php if(have_posts()){ ?>
             <?php while(have_posts()) { the_post(); ?>
-               <div class="intro-content">
-                    <section class="layout">
+                <section class="layout">
+                     <div class="intro-content">
+
                         <div class="intro-logo">
                             <?php echo '<img src="'.get_template_directory_uri().'/images/gsalborz-logo.png" />';?>
                         </div>
@@ -18,16 +19,20 @@ get_header(); ?>
                             <?php echo '<img src="'.get_template_directory_uri().'/images/gsalborz-text.png" />';?>
                         </div>
 
-                    </section>
-                </div>
-                <div class="intro-buttons">
-                    <a href="<?php echo get_post_meta(get_the_ID(),'_gsalborz_fa_link',1 ); ?>" class="intro-fa-button">
-                        <?php echo '<img src="'.get_template_directory_uri().'/images/gsalborz-ib-fa.png" />'?>
-                    </a>
-                    <a href="<?php echo get_post_meta(get_the_ID(),'_gsalborz_en_link',1 ); ?>" class="intro-en-button">
-                        <?php echo '<img src="'.get_template_directory_uri().'/images/gsalborz-ib-en.png" />'?>
-                    </a>
-                </div>
+                         <div class="intro-buttons">
+                             <a href="<?php echo get_post_meta(get_the_ID(),'_gsalborz_fa_link',1 ); ?>" class="intro-fa-button">
+                                 <?php echo '<img src="'.get_template_directory_uri().'/images/gsalborz-ib-fa.png" />'?>
+                             </a>
+                             <a href="<?php echo get_post_meta(get_the_ID(),'_gsalborz_en_link',1 ); ?>" class="intro-en-button">
+                                 <?php echo '<img src="'.get_template_directory_uri().'/images/gsalborz-ib-en.png" />'?>
+                             </a>
+                         </div>
+
+                      </div>
+
+                </section>
+
+
             <?php } ?>
 
         <?php } else { ?>
@@ -41,22 +46,22 @@ get_header(); ?>
         <?php } ?>
 
     </main>
-    <script type="text/javascript">
-        $(document).ready(function(){
-            $(".intro-content").addClass('show-intro');
-            $(".intro-buttons").addClass('show-intro');
-
-        });
-    </script>
-    <noscript>
-        <style>
-            .intro-content{
-                display : block;
-            }
-            .intro-buttons{
-                display: block;
-            }
-        </style>
-    </noscript>
+<!--    <script type="text/javascript">-->
+<!--        $(document).ready(function(){-->
+<!--            $(".intro-content").addClass('show-intro');-->
+<!--            $(".intro-buttons").addClass('show-intro');-->
+<!---->
+<!--        });-->
+<!--    </script>-->
+<!--    <noscript>-->
+<!--        <style>-->
+<!--            .intro-content{-->
+<!--                display : block;-->
+<!--            }-->
+<!--            .intro-buttons{-->
+<!--                display: block;-->
+<!--            }-->
+<!--        </style>-->
+<!--    </noscript>-->
 
 <?php get_footer(); ?>
